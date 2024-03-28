ffc72fa76f97a0b8461d6c867f11f3c5fa362dbb
Sandra Davies <dav.sandra1970@gmail.com>
2024-03-28 14:22:51 +0100

    zip-strings

---
+++ 01-zip-strings/zip-strings.js
@@ -0,0 +1,27 @@
+function zipStrings(strA, strB) {
+  let result = "";
+  let maxLengthA = strA.length;
+  let maxLengthB = strB.length;
+  let maxLength = Math.max(maxLengthA, maxLengthB);
+
+  for (let i = 0; i < maxLength; i++) {
+    if (i < maxLengthA) {
+      result += strA[i];
+    }
+    if (i < maxLengthB) {
+      result += strB[i];
+    }
+  }
+
+  return result;
+}
+
+console.log(zipStrings("abc", "123"));
+
+console.log(zipStrings("abc", "1"));
+
+console.log(zipStrings("a", "123"));
+
+console.log(zipStrings("", "123"));
+
+console.log(zipStrings("abc", ""));
